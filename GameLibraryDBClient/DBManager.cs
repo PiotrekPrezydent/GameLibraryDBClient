3f1cc2c3ffca4502bc446fd086ca9c37b9559cf0
Piotrek Prezydent <94804932+PiotrekPrezydent@users.noreply.github.com>
2025-01-07 13:12:52 +0100

    db connection

---
+++ GameLibraryDBClient/DBManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace GameLibraryDBClient
+{
+    internal static class DBManager
+    {
+        public static string IP = "";
+        public static string PORT = "";
+        public static string SERVICE = "";
+        public static string LOGIN = "";
+        public static string PASSWORD = "";
+        public static OracleConnection Client = null!;
+        public static string cst = "";
+        public static void Connect()
+        {
+            cst =$"User Id={LOGIN};Password={PASSWORD};Data Source={IP}:{PORT}/{SERVICE}";
+            Client = new OracleConnection(cst);
+            Client.Open();
+        }
+
+    }
+}
---
+++ GameLibraryDBClient/LoginWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameLibraryDBClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectBTN_Click(object sender, RoutedEventArgs e)
+        {
+            DBManager.IP = IP.Text;
+            DBManager.PORT = Port.Text;
+            DBManager.SERVICE = Service.Text;
+            DBManager.LOGIN = Login.Text;
+            DBManager.PASSWORD = Password.Text;
+
+            try
+            {
+                DBManager.Connect();
+            }
+            catch(Exception ex)
+            {
+                ConnectionStatus.Text = "BLAD W POLACZENIU Z BAZA DANYCH: " + ex.Message + "\n CONNECTION STRING:\n" + DBManager.cst;
+                return;
+            }
+            ConnectionStatus.Text = "SUKCES";
+        }
+    }
+}
